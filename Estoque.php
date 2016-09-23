850ec1eba915b05f38ce99b98e03892cdb74a033
aluno <aluno@micro000>
2016-09-22 21:13:42 -0300

    Criado projeto Estoque

---
+++ Estoque.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Gerencia os produtos em estoque
+ *
+ * @author kaudy    
+ */
+class Estoque
+{
+    private $produtos = array();
+    
+    /**
+     * Adiciona um produto a tabela
+     * @param Produto $produto
+     * @param int $quantidade
+     */
+    public function addProduto(Produto $produto, $quantidade)
+    {
+        
+    }
+    
+    /**
+     * Remove a quantidade do produto informado
+     * @param Produto $produto
+     * @param int $quantidade
+     */
+    public function remProduto(Produto $produto, $quantidade)
+    {
+        
+    }
+    
+    /**
+     * Retorna atodos os produtos em estoque
+     * @return array de Produtos
+     */
+    public function listarTudo()
+    {
+        
+    }
+    
+    /**
+     * Retorno o objeto pelo código informado
+     * @param int $codigo
+     * @return Produto
+     */
+    public function listarProduto($codigo)
+    {
+        
+    }
+}
---
+++ Produto.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Mapeia os produtos na base de dados
+ *
+ * @author kaudy 
+ */
+class Produto
+{
+    private $codigo;
+    private $nome;
+    private $tamanho;
+    private $valor;
+    private $modelo;
+    
+    
+    // Getters & Setters -------------------------------------------------------
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function getTamanho()
+    {
+        return $this->tamanho;
+    }
+
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+        return $this;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function setTamanho($tamanho)
+    {
+        $this->tamanho = $tamanho;
+        return $this;
+    }
+
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+        return $this;
+    }
+
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+        return $this;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
